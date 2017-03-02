175c3415cd3cae80e9d3f1cfcd79c28e2dd0a767
Dmitry Savenko <dsavenko@xored.com>
2017-03-02 16:33:12 +0700

    Fill app.conf with correct values automatically

--- program/dnn-desktop-demo/ck-compile.sh
+++ program/dnn-desktop-demo/ck-compile.sh
@@ -4,17 +4,3 @@ NAME="crowdsource-video-experiments-on-desktop-linux"
 rm -rf *
 unzip "../prebuilt/$NAME.zip"
 chmod +x "$CK_PROG_TARGET_EXE.sh"
-
-if [ -z "${CK_REPOS}" ]; then 
-    REPOS="$HOME/CK"
-else 
-    REPOS="$CK_REPOS"
-fi
-
-cat >app.conf <<EOF
-[General]
-ck_exe_name=./ck
-ck_bin_path=$CK_ROOT/bin
-ck_repos_path=$REPOS
-
-EOF
